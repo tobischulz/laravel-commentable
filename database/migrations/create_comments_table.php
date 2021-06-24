9dfec57298ab9ef3d80ca77d3f2e16573910c952
Tobias Schulz <tobias@byte.software>
2021-06-24 09:52:52 +0200

    fixed migration

--- database/migrations/create_comments_table.php
+++ database/migrations/create_comments_table.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Kalnoy\Nestedset\NestedSet;
+use Illuminate\Support\Facades\Schema;
 
 class CreateCommentsTable extends Migration
 {
@@ -25,7 +25,6 @@ class CreateCommentsTable extends Migration
             $table->text('body');
             $table->morphs('commentable');
             $table->morphs('creator');
-            NestedSet::columns($table);
             $table->timestamps();
         });
     }
